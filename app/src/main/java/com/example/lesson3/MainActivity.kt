20c0719f3ce477542d57bdc0b528e6a2d2481072
EliavC <eliavc2001@gmail.com>
2024-12-14 22:50:33 +0200

    board is ready with x and o just implement rules

--- app/src/main/java/com/example/lesson3/MainActivity.kt
+++ app/src/main/java/com/example/lesson3/MainActivity.kt
@@ -2,16 +2,35 @@ package com.example.lesson3
 
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.widget.Button
+import com.example.lesson3.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    enum class Turn{
+        NOUGHT,
+        CROSS
+    }
+    private var firstTurn=Turn.CROSS
+    private var currentTurn=Turn.CROSS
+
+    private var boardList = mutableListOf<Button>()
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        initBoard()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -21,6 +40,55 @@ class MainActivity : AppCompatActivity() {
         Log.e("ACTIVITY","onCreate $savedInstanceState")
     }
 
+    private fun initBoard() {
+        boardList.add(binding.a1)
+        boardList.add(binding.a2)
+        boardList.add(binding.a3)
+        boardList.add(binding.b1)
+        boardList.add(binding.b2)
+        boardList.add(binding.b3)
+        boardList.add(binding.c1)
+        boardList.add(binding.c2)
+        boardList.add(binding.c3)
+
+
+    }
+
+    fun boardTapped(view: View){
+        if(view !is Button){
+            return
+        }
+        addToBoard(view)
+    }
+    private fun addToBoard(button: Button) {
+        if(button.text!="")
+            return
+        if(currentTurn==Turn.NOUGHT){
+            button.text= NOUGHT
+            currentTurn=Turn.CROSS
+        }
+        else if(currentTurn==Turn.CROSS){
+            button.text= CROSS
+            currentTurn=Turn.NOUGHT
+    }
+        setTurnLabel()
+    }
+
+    private fun setTurnLabel() {
+        var turnText=""
+        if(currentTurn==Turn.CROSS){
+            turnText="Turn$CROSS"
+        }
+        else if(currentTurn==Turn.NOUGHT){
+            turnText="Turn$NOUGHT"
+        }
+        binding.turnTV.text=turnText
+    }
+
+    companion object{
+        const val CROSS="X"
+        const val NOUGHT="0"
+    }
     override fun onResume() {
         super.onResume()
         Log.e("ACTIVITY","onResume")
@@ -51,3 +119,4 @@ class MainActivity : AppCompatActivity() {
         Log.e("ACTIVITY","onDestroy")
     }
 }
+
